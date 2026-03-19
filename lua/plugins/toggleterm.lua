f0c2ba6f7125e33ec6e316bb34fa718ec74db89d
Goutham Krishna Shabu <gouthamshabu@gmail.com>
2026-03-18 23:24:00 -0600

    feat: add toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+---@module 'lazy'
+---@type LazySpec
+---akinsho/toggleterm.nvim
+return {
+  'akinsho/toggleterm.nvim',
+  config = true,
+  keys = {
+    { '<leader>t', '<cmd>ToggleTerm<cr>', desc = 'Toggle Terminal' },
+  },
+}
